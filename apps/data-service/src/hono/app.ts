142e285276a03261faf827ddc7f450d09c09c1f2
Kyle Wybranowski <kylewybranowski@Kyles-Mac-mini.local>
2025-11-12 17:39:56 -0500

    Checkpoint: Smart Routing Service

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,11 +1,25 @@
 import { getLink } from '@repo/data-ops/queries/links';
 import { Hono } from 'hono';
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { getDestinationForCountry, getRoutingDestinations } from '@/helpers/route-ops';
 
 export const App = new Hono<{ Bindings: Env }>();
 
 App.get('/:id', async (c) => {
-	console.log(JSON.stringify(c.req.raw.cf));
 	const id = c.req.param('id');
-	const linkInfoFromDb = await getLink(id);
-	return c.json(linkInfoFromDb);
+
+	const linkInfo = await getRoutingDestinations(c.env, id);
+	if (!linkInfo) {
+		return c.text('Destination not found', 404);
+	}
+
+	const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf);
+	if (!cfHeader.success) {
+		return c.text('Invalid Cloudflare header', 400);
+	}
+
+	const headers = cfHeader.data;
+	const destination = getDestinationForCountry(linkInfo, headers.country);
+
+	return c.redirect(destination);
 });
